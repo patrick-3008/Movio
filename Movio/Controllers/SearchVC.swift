cc67efc1f1535b8d28ce2731fd6c4dab2497e23e
Patrick <patricknaashat@yahoo.com>
2024-03-06 22:52:00 +0200

    add tableview in search screen

--- Movio/Controllers/SearchVC.swift
+++ Movio/Controllers/SearchVC.swift
@@ -8,11 +8,41 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    private let discoverTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
 
         view.backgroundColor = .systemBackground
+        view.addSubview(discoverTable)
+        discoverTable.delegate = self
+        discoverTable.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
     }
 
+}
+
+// MARK: - for tables
+extension SearchVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else { return UITableViewCell()}
+        return cell
+    }
 }
