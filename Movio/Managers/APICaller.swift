413971bcb1840f4e9ff24efc5cb89181b71cf4b6
Patrick <patricknaashat@yahoo.com>
2024-03-07 18:51:27 +0200

    add youtube api

--- Movio/Managers/APICaller.swift
+++ Movio/Managers/APICaller.swift
@@ -10,6 +10,8 @@ import Foundation
 struct Constants {
     static let API_KEY = "697d439ac993538da4e3e60b54e762cd"
     static let baseURL = "https://api.themoviedb.org"
+    static let YoutubeAPI_KEY = "AIzaSyD848OqZPhSxRjyNBXkQRiL_vnkpLu1_UU"
+    static let YoutubeBaseURL = "https://youtube.googleapis.com/youtube/v3/search?"
 }
 
 enum APIError: Error {
@@ -143,6 +145,25 @@ class APICaller {
             }
             
         }
+        task.resume()
+    }
+    
+    func getMovies(with query: String){
+        
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return }
+        guard let url = URL(string: "\(Constants.YoutubeBaseURL)q=\(query)&key=\(Constants.YoutubeAPI_KEY)") else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {return}
+            
+            do {
+                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                print(results)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
         task.resume()
     }
 }
