21261ed5af8a0d088a7696be8a8aac61ba4e8c6b
Patrick <patricknaashat@yahoo.com>
2024-03-08 14:20:38 +0200

    configure hero view with random movie

--- Movio/Controllers/Core/HomeVC.swift
+++ Movio/Controllers/Core/HomeVC.swift
@@ -17,6 +17,9 @@ enum Sections: Int {
 
 class HomeVC: UIViewController {
     
+    private var randomTrendingMovie: Title?
+    private var headerView: HeroHeaderUIView?
+    
     let sectionTitles: [String] = ["Trending Movies", "Trending Tv", "Popular", "Upcoming Movies", "Top rated"]
     
     private let homeFeedTable: UITableView = {
@@ -36,8 +39,25 @@ class HomeVC: UIViewController {
         
         configureNavBar()
         
-        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
+        
+        configureHeroHeaderView()
+    }
+    
+    private func configureHeroHeaderView() {
+        APICaller.shared.getTerndingMovies { [weak self] result in
+            switch result {
+                case .success(let titles):
+                    let selectedTitle = titles.randomElement()
+                    self?.randomTrendingMovie = selectedTitle
+                    
+                    self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_name ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+            }
+        }
     }
     
     private func configureNavBar() {
