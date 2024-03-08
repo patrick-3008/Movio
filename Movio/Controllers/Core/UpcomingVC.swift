83b6ef339835ec64ddcf1339243b5c83d273494f
Patrick <patricknaashat@yahoo.com>
2024-03-08 14:29:05 +0200

    add title preview when clicked in both coming soon and search screens

--- Movio/Controllers/Core/UpcomingVC.swift
+++ Movio/Controllers/Core/UpcomingVC.swift
@@ -10,16 +10,16 @@ import UIKit
 class UpcomingVC: UIViewController {
     
     private var titles: [Title] = [Title]()
-
+    
     private let upcomingTable: UITableView = {
         let table = UITableView()
         table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
         return table
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
         title = "Upcoming"
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -61,7 +61,7 @@ extension UpcomingVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else { return UITableViewCell() }
-
+        
         let title = titles[indexPath.row]
         cell.configure(with: TitleViewModel(titleName: title.original_title ?? title.original_name ?? "Unknown Title Name", posterURL: title.poster_path ?? ""))
         return cell
@@ -70,4 +70,29 @@ extension UpcomingVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let title = titles[indexPath.row]
+        
+        guard let titleName = title.original_title ?? title.original_name else { return }
+        
+        APICaller.shared.getMovie(with: titleName) { [weak self] result in
+            switch result {
+                case .success(let videoElement):
+                    
+                    DispatchQueue.main.async {
+                        let vc = TitlePreviewVC()
+                        vc.configure(with: TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverview: title.overview ?? ""))
+                        
+                        self?.navigationController?.pushViewController(vc, animated: true)
+                    }
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+            }
+        }
+    }
+    
 }
